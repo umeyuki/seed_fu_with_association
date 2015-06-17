da4230161fcca123326f86574567c9bccc8bc6d0
umeyuki <umeyuki1326+github@gmail.com>
2015-06-17 14:03:21 +0900

    Add delete query before book_author fixture

--- db/fixtures/03_book_author.rb
+++ db/fixtures/03_book_author.rb
@@ -1,3 +1,7 @@
+# seed-fu外で挿入されたレコードは重複するため削除する
+# sqlite3がTRUNCATEサポートしてないのでDELETE
+ActiveRecord::Base.connection.execute(  "DELETE FROM book_authors;")
+
 author1 = Author.find_by_name("Hiromitsu Ito")
 author1.books << Book.find_by_name("Book 1")
 author1.books << Book.find_by_name("Book 2")
