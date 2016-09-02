496a0b1a0f26772ad4fd722227514b7af4d8f045
umeyuki <umeyuki1326+github@gmail.com>
2016-09-02 16:56:23 +0900

    Change seed_fu fixtures with association

--- db/fixtures/03_book_author.rb
+++ db/fixtures/03_book_author.rb
@@ -1,7 +1,11 @@
-# seed-fu外で挿入されたレコードは重複するため削除する
-# sqlite3がTRUNCATEサポートしてないのでDELETE
-ActiveRecord::Base.connection.execute(  "DELETE FROM book_authors;")
+BookAuthor.seed do |s|
+  s.id = 1
+  s.book = Book.find_by(name: "Book 1")
+  s.author = Author.find_by(name: "Hiromitsu Ito")
+end
 
-author1 = Author.find_by_name("Hiromitsu Ito")
-author1.books << Book.find_by_name("Book 1")
-author1.books << Book.find_by_name("Book 2")
+BookAuthor.seed do |s|
+  s.id = 2
+  s.book = Book.find_by(name: "Book 2")
+  s.author = Author.find_by(name: "Hiromitsu Ito")
+end
